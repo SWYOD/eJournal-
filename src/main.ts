bc26332fa24f326f4bde96168c0be95b63d20132
Andrey Pankov <sob.dod@gmail.com>
2025-02-28 22:01:29 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
       .setVersion('1.0')
       .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
+  app.enableCors();
   SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
